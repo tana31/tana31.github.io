0a71d6d979b6ed7c6a27b5d3d46b2854236e76f5
H T <lowsound.tohky.82@gmail.com>
2022-01-22 20:55:20 +0900

    レスポンシブ対応

--- user_script/wishlist.js
+++ user_script/wishlist.js
@@ -10,23 +10,39 @@ function main() {
 
     function csvread(){
         let input_text = document.getElementById('input').innerHTML;
+
+        // 改行コードで分割し、配列に格納
         let csv_table = input_text.trim().split("\n");
-        let result = [];
-            for (let tr of csv_table){
-                tr = tr.trim().split(",");
-                result.push(tr);
-            }
+
+        // 二次元配列生成
+        let dim_array = [];
+        for (let tr of csv_table){
+            tr = tr.trim().split(",");
+            dim_array.push(tr);
+        }
+        // タイトル行削除、見出し
+        const sp_h = dim_array.shift();
+
+        // HTML生成
         let html = '';
-        for (let tr of result){
-            html += '<tr>'
-            for (let td of tr){
-                html += '<td>' + td + '</td>';
-            }
-            html += '</tr>'
+        html += '<table class="tbl-r05">'
+        for(let i=2;i<sp_h.length;i++){
+            html += `<th>${sp_h[i]}</th>`;
         }
 
+        for (let tr of dim_array){
+            // const place = tr[3];
+            // const pref = tr[2];
+            html += "<tr>";
+            for (let i=2;i<tr.length;i++){
+                html += `<td data-label="${sp_h[i]}">${tr[i]}</td>`;
+            }
+            html += "</tr>";
+        }
+        html += '</table>'
+
         let output = document.getElementById('output');
-        output.innerHTML = '<table>' + html + '</table>';
+        output.innerHTML = html;
     }
 
 
